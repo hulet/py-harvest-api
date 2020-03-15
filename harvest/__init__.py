1e168e8fbecaee90e643cab93a452b6130422aeb
Ramon Moraes <ramon@vyscond.io>
2020-03-15 20:33:04 -0300

    Updating setup and remove doc strings from API

--- harvest/__init__.py
+++ harvest/__init__.py
@@ -5,4 +5,4 @@ LOGLEVEL = os.environ.get("HARVEST_LOGLEVEL", "INFO")
 logging.basicConfig(level=LOGLEVEL)
 logger = logging.getLogger("Harvest API")
 
-__version__ = "0.1.0a1"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup
 from harvest import __version__
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
-    name='harvest',
-    author="Ramon Moraes",
-    author_email="ramon@vyscond.io",
+    name='harvest_api',
+    author='Ramon Moraes',
+    author_email='ramon@vyscond.io',
     version=__version__,
     description='Harvest API',
-    long_description="".join(open("README.md")),
-    url="https://github.com/vyscond/py-harvest-api",
-    license="MIT",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vyscond/py-harvest-api',
+    license='MIT',
     packages=[
-        "harvest"
+        'harvest',
     ],
     install_requires=[
-        'requests==2.19.1'
+        'requests>=2.19.1',
     ],
 )
-
