910b0546cf74835c1157d571cbb4c34dd5eef637
Ramon Moraes <ramon@vyscond.io>
2020-04-27 18:51:09 -0300

    Fixes #9

--- harvest/__init__.py
+++ harvest/__init__.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 LOGLEVEL = os.environ.get("HARVEST_LOGLEVEL", "INFO")
-logging.basicConfig(level=LOGLEVEL)
 logger = logging.getLogger("Harvest API")
+logger.setLevel(LOGLEVEL)
 
 __version__ = "0.2.0"
