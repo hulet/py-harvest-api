1a29a4e3dc6f91b3ced47d913f33d9f0ca5fba1e
Ramon Moraes <ramon@vyscond.io>
2020-04-27 18:55:14 -0300

    Fixes #10

--- harvest/__init__.py
+++ harvest/__init__.py
@@ -1,8 +1,10 @@
 import os
 import logging
+__version__ = '0.2.0'
+__name__ = 'harvest_api'
+__author__ = 'Ramon Moraes'
+__author_email__ = 'ramonmoraes8080@gmail.com'
 
 LOGLEVEL = os.environ.get("HARVEST_LOGLEVEL", "INFO")
 logger = logging.getLogger("Harvest API")
 logger.setLevel(LOGLEVEL)
-
-__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup
 from harvest import __version__
+from harvest import __name__
+from harvest import __author__
+from harvest import __author_email__
+
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+
 setup(
-    name='harvest_api',
-    author='Ramon Moraes',
-    author_email='ramon@vyscond.io',
+    name=__name__,
+    author=__author__,
+    author_email=__author_email__,
     version=__version__,
     description='Harvest API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/vyscond/py-harvest-api',
+    url='https://github.com/velvetkeyboard/py-harvest-api',
     license='MIT',
     packages=[
         'harvest',
